37ebb4e97d39c0bd7824e1403c45a5b651250260
Dominika Umiastowska <62251932+umiastowskad@users.noreply.github.com>
2022-12-06 17:33:56 +0100

    zmiany

--- PROJECTS/Music_genre_classification-bad/project1/code/nlp_classifier.py
+++ PROJECTS/Music_genre_classification-bad/project1/code/nlp_classifier.py
@@ -5,23 +5,27 @@ from abc import ABC, abstractmethod
 from joblib import dump, load
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import SGDClassifier
-from sklearn import preprocessing
 from tensorflow.keras import layers, models
+from xgboost import XGBClassifier
+tf.get_logger().setLevel('ERROR')
 
 class NLPClassifier(ABC):
-    def __init__(self):
+    def __init__(self, ):
         self.model = None
         self.name = None
-    
+
     def partial_fit(self, X, Y, classes):
         self.model.partial_fit(X, Y, classes)
-    
+
     def predict(self, X):
         return self.model.predict(X)
-    
+
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
+
     def save(self, filename):
         dump(self.model, f'{filename}.joblib')
-    
+
     def load(self, filename):
         self.model = load(filename)
 
@@ -36,41 +40,43 @@ class SVM(NLPClassifier):
         self.model = SGDClassifier()
         self.name = 'svm'
 
-class XGBoost(NLPClassifier):    
+class XGBoost(NLPClassifier):
     def __init__(self, class_count, boost_iter=30):
         self.model = None
         self.name = 'xgboost'
         self.params = {'objective': 'multi:softmax', 'num_class': class_count}
         self.boost_iter = boost_iter
-    
+
     def partial_fit(self, X, Y, classes):
         data = xgb.DMatrix(X, label=Y)
         self.model = xgb.train(self.params, data, self.boost_iter, xgb_model=self.model)
-    
+
     def predict(self, X):
         return self.model.predict(xgb.DMatrix(X)).astype(int)
-    
+
     def save(self, filename):
         self.model.save_model(f'{filename}.json')
-    
+
     def load(self, filename):
         self.model = xgb.Booster()
         self.model.load_model(filename)
 
 class CNN(NLPClassifier):
-    def __init__(self, vec_len, class_count, optimizer):
+    def __init__(self, vec_len, class_count, optimizer, epochs=1):
         self.name = 'cnn'
+        self.epochs = epochs
 
         model = models.Sequential()
         model.add(layers.Conv1D(8, 3, padding='same', activation='relu', input_shape=(vec_len, 1)))
         model.add(layers.MaxPooling1D(2, strides=2))
+        model.add(layers.Dropout(0.05))
         model.add(layers.Conv1D(16, 3, padding='same', activation='relu'))
         model.add(layers.MaxPooling1D(2, strides=2))
+        model.add(layers.Dropout(0.05))
         model.add(layers.Conv1D(32, 3, padding='same', activation='relu'))
-
         model.add(layers.Flatten())
         model.add(layers.Dense(128, activation='relu'))
-        model.add(layers.Dense(class_count))
+        model.add(layers.Dense(class_count, activation='softmax'))
 
         model.compile(optimizer=optimizer,
                       loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -81,12 +87,16 @@ class CNN(NLPClassifier):
     def partial_fit(self, X, Y, classes):
         X_proc = X.reshape(*X.shape, 1)
         Y_proc = Y.reshape(-1, 1)
-        self.model.fit(X_proc, Y_proc)
+        self.model.fit(X_proc, Y_proc, epochs=self.epochs)
 
     def predict(self, X):
         pred = np.argmax(self.model.predict(X.reshape(*X.shape, 1)), axis=1)
         return pred.flatten()
 
+    def predict_proba(self, X):
+        pred = self.model.predict(X.reshape(*X.shape, 1))
+        return pred.flatten()
+    
     def save(self, filename):
         self.model.save(filename)
 
--- PROJECTS/Music_genre_classification-bad/project1/code/nlp_embedding.py
+++ PROJECTS/Music_genre_classification-bad/project1/code/nlp_embedding.py
@@ -25,3 +25,48 @@ class GloVe(NLPEmbedding):
     def __process_embedding(self, embedding):
         embedding.resize((self.max_words, self.embedding_size), refcheck=False)
         return embedding.flatten()
+
+class SmallBert(NLPEmbedding):
+    def __init__(self, max_words):
+        self.model = nlu.load('bert')
+        self.name = 'bert'
+        self.max_words = max_words
+        self.embedding_size = 128
+
+    def embed_lyrics(self, data):
+        embeddings = self.model.predict(data, output_level='document')
+        return np.array([self.__process_embedding(embedding) for embedding in embeddings.word_embedding_bert])
+    
+    def __process_embedding(self, embedding):
+        embedding.resize((self.max_words, self.embedding_size), refcheck=False)
+        return embedding.flatten()
+
+class Bert(NLPEmbedding):
+    def __init__(self, max_words):
+        self.model = nlu.load('en.embed.bert')
+        self.name = 'big-bert'
+        self.max_words = max_words
+        self.embedding_size = 768
+
+    def embed_lyrics(self, data):
+        embeddings = self.model.predict(data, output_level='document')
+        return np.array([self.__process_embedding(embedding) for embedding in embeddings.word_embedding_bert])
+
+    def __process_embedding(self, embedding):
+        embedding.resize((self.max_words, self.embedding_size), refcheck=False)
+        return embedding.flatten()
+
+class Word2vec(NLPEmbedding):
+    def __init__(self, max_words):
+        self.model = nlu.load('en.embed.word2vec.gigaword')
+        self.name = 'word2vec'
+        self.max_words = max_words
+        self.embedding_size = 300
+
+    def embed_lyrics(self, data):
+        embeddings = self.model.predict(data, output_level='document')
+        return np.array([self.__process_embedding(embedding) for embedding in embeddings.word_embedding_UNIQUE])
+
+    def __process_embedding(self, embedding):
+        embedding.resize((self.max_words, self.embedding_size), refcheck=False)
+        return embedding.flatten()
