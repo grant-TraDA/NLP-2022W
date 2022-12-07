356065913bb879fe6568f12de3c36a288faf08ea
Paweł Golik <01142171@pw.edu.pl>
2022-12-07 22:25:22 +0100

    adding pretraining and embedding extraction

---
+++ PROJECTS/E-commerce products_MOP/source/config.py
@@ -0,0 +1,10 @@
+from os import path
+
+# paths
+BASE_PATH = path.join(r'C:\Users\golik\Desktop\mgr\nlp\\')
+REPO_PATH = path.join(BASE_PATH, r'NLP-2022W-MOP\PROJECTS\E-commerce products_MOP\\')
+EMBEDDING_PATH = path.join(REPO_PATH, r'embeddings\30-11-2022\\')
+SIMILARITY_PATH = path.join(REPO_PATH, r'similarity\30-11-2022\\')
+PRETRAIN_OUTPUT_PATH = path.join(REPO_PATH, r'pretraining_output\30-11-2022\\')
+
+
