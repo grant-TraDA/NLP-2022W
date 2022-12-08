391021d286da0ae266450ca4e1a67a0793d66d7b
Mateusz Jastrzębiowski <m.jastrzebiowski@gmail.com>
2022-12-09 00:43:55 +0100

    Probing tasks tests

--- PROJECTS/E-commerce products_MOP/source/utils/probing_tasks_utils.py
+++ PROJECTS/E-commerce products_MOP/source/utils/probing_tasks_utils.py
@@ -1,65 +1,28 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
-from sklearn import preprocessing
-from sklearn.metrics import f1_score
-
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
+import warnings
 
+warnings.filterwarnings("ignore")
 import numpy as np
 
 
-def test_probing_task_xgb(train, test):
+def test_probing_task(train, test, clf):
 
-    """
-    Returns: returns predictions, accuracy
-    """
-    le = preprocessing.LabelEncoder()
-    train_labels = le.fit_transform(train.iloc[:, -1])
-    test_labels = le.transform(test.iloc[:, -1])
+    X_train, y_train = (train.drop(["label"], axis=1), np.array(train["label"]))
+    X_test, y_test = (test.drop(["label"], axis=1), np.array(test["label"]))
 
-    model_1 = XGBClassifier()
-    model_1.fit(train.iloc[:, :-1], train_labels)
-    pred_1 = model_1.predict(test.iloc[:, :-1])
-
-    return pred_1  # , accuracy_score(y_true=test_labels, y_pred=predictions)
-
-
-def test_probing_task_random_forest(train, test):
-
-    """
-    Returns: returns predictions, accuracy
-    """
-    X_train, y_train = train.drop(["label"], axis=1), np.array(train["label"])
-    X_test, y_test = test.drop(["label"], axis=1), np.array(test["label"])
-
-    clf = RandomForestClassifier(random_state=42)
-    clf.fit(X_train, y_train)
+    if type(clf) == XGBClassifier:
+        clf.fit(X_train, y_train, eval_metric="mlogloss")
+    else:
+        clf.fit(X_train, y_train)
 
     predictions = clf.predict(X_test)
+    acc = accuracy_score(y_true=y_test, y_pred=predictions)
+    f_score = f1_score(y_true=y_test, y_pred=predictions, average="macro")
 
-    return (
-        predictions,
-        accuracy_score(y_true=y_test, y_pred=predictions),
-        f1_score(y_true=y_test, y_pred=predictions, average="macro"),
-    )
+    print(f"Accuracy: {acc}, f_score: {f_score}")
 
-
-def test_probing_task_logistic_regression(train, test):
-    X_train, y_train = train.drop(["label"], axis=1), np.array(train["label"])
-    X_test, y_test = test.drop(["label"], axis=1), np.array(test["label"])
-
-    clf = LogisticRegression(
-        multi_class="multinomial", random_state=42, penalty="l1", solver="saga"
-    )
-
-    clf.fit(X_train, y_train)
-
-    predictions = clf.predict(X_test)
-
-    return (
-        predictions,
-        accuracy_score(y_true=y_test, y_pred=predictions),
-        f1_score(y_true=y_test, y_pred=predictions, average="macro"),
-    )
+    return (predictions, acc, f_score)
