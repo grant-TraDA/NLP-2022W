cbccc14278c686cb3cd68d7fa7f063c7545a9a98
Aleksandra Muszkowska <aleksandra.muszkowska@gmail.com>
2022-12-11 19:55:48 +0100

    probing tasks

--- PROJECTS/E-commerce products_MOP/source/config.py
+++ PROJECTS/E-commerce products_MOP/source/config.py
@@ -1,10 +1,9 @@
 from os import path
 
 # paths
-BASE_PATH = path.join(r'C:\Users\golik\Desktop\mgr\nlp\\')
-REPO_PATH = path.join(BASE_PATH, r'NLP-2022W-MOP\PROJECTS\E-commerce products_MOP\\')
-EMBEDDING_PATH = path.join(REPO_PATH, r'embeddings\30-11-2022\\')
-SIMILARITY_PATH = path.join(REPO_PATH, r'similarity\30-11-2022\\')
-PRETRAIN_OUTPUT_PATH = path.join(REPO_PATH, r'pretraining_output\30-11-2022\\')
-
+BASE_PATH = path.join("C:/Users/aleks/Desktop/SEM_3/nlp/project1_repo/")
+REPO_PATH = path.join(BASE_PATH, "NLP-2022W-MOP/PROJECTS/E-commerce products_MOP/")
+EMBEDDING_PATH = path.join(REPO_PATH, "embeddings/30-11-2022/")
+SIMILARITY_PATH = path.join(REPO_PATH, r"similarity\30-11-2022\\")
+PRETRAIN_OUTPUT_PATH = path.join(REPO_PATH, r"pretraining_output\30-11-2022\\")
 
