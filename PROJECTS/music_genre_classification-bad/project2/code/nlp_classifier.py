94c0704f0c526773afb1adfc6ed107fb9db1b9c4
alexnawr <79359626+alexnawr@users.noreply.github.com>
2023-01-05 22:08:04 +0100

    code review

--- PROJECTS/music_genre_classification-bad/project2/code/nlp_classifier.py
+++ PROJECTS/music_genre_classification-bad/project2/code/nlp_classifier.py
@@ -8,61 +8,7 @@ from sklearn.linear_model import SGDClassifier
 from tensorflow.keras import layers, models
 tf.get_logger().setLevel('ERROR')
 
-class NLPClassifier(ABC):
-    def __init__(self, classes):
-        self.model = None
-        self.name = None
-        self.classes = classes
-
-    def partial_fit(self, X, Y):
-        self.model.partial_fit(X, Y, self.classes)
-
-    def predict(self, X):
-        return self.model.predict(X)
-
-    def predict_proba(self, X):
-        return self.model.predict_proba(X)
-
-    def save(self, filename):
-        dump(self.model, f'{filename}.joblib')
-
-    def load(self, filename):
-        self.model = load(filename)
-
-
-class NaiveBayes(NLPClassifier):
-    def __init__(self):
-        self.model = GaussianNB()
-        self.name = 'naive-bayes'
-
-class SVM(NLPClassifier):
-    def __init__(self):
-        self.model = SGDClassifier()
-        self.name = 'svm'
-
-class XGBoost(NLPClassifier):
-    def __init__(self, class_count, boost_iter=30):
-        self.model = None
-        self.name = 'xgboost'
-        self.params = {'objective': 'multi:softmax', 'num_class': class_count}
-        self.boost_iter = boost_iter
-
-    def partial_fit(self, X, Y):
-        data = xgb.DMatrix(X, label=Y)
-        self.model = xgb.train(self.params, data, self.boost_iter, xgb_model=self.model)
-
-    def predict(self, X):
-        return self.model.predict(xgb.DMatrix(X)).astype(int)
-
-    def save(self, filename):
-        self.model.save_model(f'{filename}.json')
-
-    def load(self, filename):
-        self.model = xgb.Booster()
-        self.model.load_model(filename)
-
-class CNNClasifier(NLPClassifier):
-
+class CNNClassifier:
     def partial_fit(self, X, Y):
         X_proc = X.reshape(*X.shape, 1)
         Y_proc = Y.reshape(-1, 1)
@@ -82,7 +28,8 @@ class CNNClasifier(NLPClassifier):
     def load(self, filename):
         self.model = models.load_model(filename)
 
-class CNN(CNNClasifier):
+
+class CNN(CNNClassifier):
     def __init__(self, vec_len, class_count, optimizer):
         self.name = 'cnn'
 
@@ -104,7 +51,7 @@ class CNN(CNNClasifier):
 
         self.model = model
 
-class BinaryCNN(CNNClasifier):
+class BinaryCNN(CNNClassifier):
     def __init__(self, vec_len, optimizer):
         self.name = 'binary_cnn'
 
@@ -130,3 +77,40 @@ class BinaryCNN(CNNClasifier):
 
     def predict_proba(self, X):
         pass
+
+class CNN2Step(CNNClassifier):
+    def __init__(self, vec_len, class_count, optimizer, indiv_class):
+        self.name = '2_step_cnn'
+        self.model1 = BinaryCNN(vec_len, optimizer)
+        self.model2 = CNN(vec_len, class_count - 1, optimizer)
+        self.indiv_class = indiv_class
+
+    def partial_fit(self, X, Y):
+        Y_binary = np.array(Y == self.indiv_class).astype(int)
+        self.model1.partial_fit(X.reshape(*X.shape, 1), Y_binary.reshape(-1, 1))
+        
+        X_other = X[Y != self.indiv_class]
+        Y_other = Y[Y != self.indiv_class]
+        self.model2.partial_fit(X_other.reshape(*X_other.shape, 1), Y_other.reshape(-1, 1))
+
+    def predict(self, X):
+        pred1 = self.model1.predict(X.reshape(*X.shape, 1))
+        X2 = X[pred1 == 0]
+        pred2 = self.model2.predict(X2.reshape(*X2.shape, 1))
+        
+        pred = np.zeros(len(X), dtype=int)
+        pred[pred1 == 1] = self.indiv_class
+        pred[pred1 == 0] = pred2
+        
+        return pred.flatten()
+
+    def predict_proba(self, X):
+        pass
+    
+    def save(self, filename):
+        self.model1.save(f'{filename}1')
+        self.model2.save(f'{filename}2')
+
+    def load(self, filename):
+        self.model1 = models.load_model(f'{filename}1')
+        self.model2 = models.load_model(f'{filename}2')
