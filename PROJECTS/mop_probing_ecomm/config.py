f14fa46ab149529f67e3378a21f4644cbc6fa85a
Aleksandra Muszkowska <aleksandra.muszkowska@gmail.com>
2023-01-19 16:08:11 +0100

    update files structure

--- PROJECTS/mop_probing_ecomm/config.py
+++ PROJECTS/mop_probing_ecomm/config.py
@@ -2,14 +2,14 @@ from os import path
 
 # paths
 BASE_PATH = path.join("C:/Users/aleks/Desktop/SEM_3/nlp/project1_repo/")
-REPO_PATH = path.join(BASE_PATH, "NLP-2022W-MOP/PROJECTS/E-commerce products_MOP/")
-EMBEDDING_PATH = path.join(REPO_PATH, "embeddings/30-11-2022/")
+REPO_PATH = path.join(BASE_PATH, "NLP-2022W-MOP/PROJECTS/mop_probing_ecomm/")
+EMBEDDING_PATH = path.join(REPO_PATH, "datasets/project2/computers/embeddings")
+
 SIMILARITY_PATH = path.join(REPO_PATH, r"similarity\30-11-2022\\")
 PRETRAIN_OUTPUT_PATH = path.join(REPO_PATH, r"pretraining_output\30-11-2022\\")
 
 # dataset params
 
-TYPE = 'cameras'
-SIZE = 'medium'
-
+TYPE = "computers"
+SIZE = "medium"
 
