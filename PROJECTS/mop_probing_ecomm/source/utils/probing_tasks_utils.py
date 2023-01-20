f2c857c6ae4e479c06d22537dc3e22c072d13404
Aleksandra Muszkowska <aleksandra.muszkowska@gmail.com>
2023-01-20 02:44:42 +0100

    add gitkeep

--- PROJECTS/mop_probing_ecomm/source/utils/probing_tasks_utils.py
+++ PROJECTS/mop_probing_ecomm/source/utils/probing_tasks_utils.py
@@ -1,18 +1,12 @@
-from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 import warnings
 
 warnings.filterwarnings("ignore")
-import numpy as np
 
 
-def test_probing_task(train, test, clf):
-
-    X_train, y_train = (train.drop(["label"], axis=1), np.array(train["label"]))
-    X_test, y_test = (test.drop(["label"], axis=1), np.array(test["label"]))
+def test_probing_task(X_train, X_test, y_train, y_test, clf):
 
     if type(clf) == XGBClassifier:
         clf.fit(X_train, y_train, eval_metric="mlogloss")
@@ -23,6 +17,6 @@ def test_probing_task(train, test, clf):
     acc = accuracy_score(y_true=y_test, y_pred=predictions)
     f_score = f1_score(y_true=y_test, y_pred=predictions, average="macro")
 
-    print(f"Accuracy: {acc}, f_score: {f_score}")
+    # print(f"Accuracy: {acc}, f_score: {f_score}")
 
     return (predictions, acc, f_score)
